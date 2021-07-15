faebed39a3a482496cd2c4772af2380d546d0aae
Sanjayshrestha98 <sanjayshrestha969@gmail.com>
2021-07-15 14:51:52 +0545

    added list view for product datatable

--- src/AdminContainer/AdminContainer.js
+++ src/AdminContainer/AdminContainer.js
@@ -7,6 +7,7 @@ import AdminSideNav from './AdminSideNav';
 import Orderlist from './Orderlist';
 import ViewUser from './User/ViewUser';
 import AddUser from './User/AddUser';
+import ViewProduct from './Product/ViewProduct'
 
 const AdminContainer = () => {
 
@@ -23,6 +24,8 @@ const AdminContainer = () => {
                 <Route path = "/admin/order" component = {Orderlist}/>
                 <Route path = "/admin/viewuser" component = {ViewUser}/>
                 <Route path = "/admin/adduser" component= {AddUser}/>
+
+                <Route path = "/admin/viewproduct" component= {ViewProduct}/>
             </div>
         </>
     )
--- src/AdminContainer/User/ViewUser.js
+++ src/AdminContainer/User/ViewUser.js
@@ -1,98 +1,125 @@
 import React from 'react';
-import { MDBDataTable } from 'mdbreact';
+import { MDBDataTable, } from 'mdbreact';
 
 
 function ViewUser() {
-    const data = {
-        columns: [
-          {
-            label: 'Fullname',
-            field: 'fullname',
-            sort: 'asc',
-            width: 150
-          },
-          {
-            label: 'Gender',
-            field: 'gender',
-            sort: 'asc',
-            width: 270
-          },
-          {
-            label: 'Email',
-            field: 'email',
-            sort: 'asc',
-            width: 200
-          },
-          {
-            label: 'Phone',
-            field: 'phone',
-            sort: 'asc',
-            width: 100
-          },
-          {
-            label: 'Address',
-            field: 'address',
-            sort: 'asc',
-            width: 150
-          },
-          {
-            label: 'Username',
-            field: 'username',
-            sort: 'asc',
-            width: 100
-          }
-        ],
-        rows: [
-          {
-            fullname: 'Sanjay Shrestha',
-            gender: 'Male',
-            email: 'sanjayshrestha969@gmail.com',
-            phone: '9861774998',
-            address: 'Kathmandu',
-            username: 'shrestha45'
-          },
-          {
-            fullname: 'Safall Shakya',
-            gender: 'Male',
-            email: 'safall@gmail.com',
-            phone: '9861281474',
-            address: 'Baneshwor',
-            username: 'safall12'
-          },
-          {
-            fullname: 'Sariyan Magar',
-            gender: 'Female',
-            email: 'sariyanmagar@gmail.com',
-            phone: '9861908460',
-            address: 'Gokarna',
-            username: 'sariyan09'
-          }, 
-          {
-            fullname: 'Nikita Ojha',
-            gender: 'Female',
-            email: 'nikita2018@gmail.com',
-            phone: '9811276221',
-            address: 'Biratnagar',
-            username: 'ojha2018'
-          }, 
-          {
-            fullname: 'Bhumika Lama',
-            gender: 'Female',
-            email: 'bhumikalama@gmail.com',
-            phone: '9811276221',
-            address: 'Swayambhu',
-            username: 'bhumikalama44'
-          }, 
-        ]
-      };
+  const data = {
+    columns: [
+      {
+        label: 'Fullname',
+        field: 'fullname',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: 'Gender',
+        field: 'gender',
+        sort: 'asc',
+        width: 270
+      },
+      {
+        label: 'Email',
+        field: 'email',
+        sort: 'asc',
+        width: 200
+      },
+      {
+        label: 'Phone',
+        field: 'phone',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Address',
+        field: 'address',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: 'Username',
+        field: 'username',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label:'Action',
+        field:'action',
+        width:100
+      }
+    ],
+    rows: [
+      {
+        fullname: 'Sanjay Shrestha',
+        gender: 'Male',
+        email: 'sanjayshrestha969@gmail.com',
+        phone: '9861774998',
+        address: 'Kathmandu',
+        username: 'shrestha45',
+        action:<div><button className="editbtn"> editbtn </button> <button className="deletebtn"> deletebtn </button> </div>,
+
+      },
+      {
+        fullname: 'Safall Shakya',
+        gender: 'Male',
+        email: 'safall@gmail.com',
+        phone: '9861281474',
+        address: 'Baneshwor',
+        username: 'safall12',
+        action:<div><button className="editbtn"> editbtn </button> <button className="deletebtn"> deletebtn </button> </div>,
+        
+
+      },
+      {
+        fullname: 'Sariyan Magar',
+        gender: 'Female',
+        email: 'sariyanmagar@gmail.com',
+        phone: '9861908460',
+        address: 'Gokarna',
+        username: 'sariyan09',
+        action:<div><button className="editbtn"> editbtn </button> <button className="deletebtn"> deletebtn </button> </div>,
+
+
+      },
+      {
+        fullname: 'Nikita Ojha',
+        gender: 'Female',
+        email: 'nikita2018@gmail.com',
+        phone: '9811276221',
+        address: 'Biratnagar',
+        username: 'ojha2018',
+        action:<div><button className="editbtn"> editbtn </button> <button className="deletebtn"> deletebtn </button> </div>,
+
+
+      },
+      {
+        fullname: 'Bhumika Lama',
+        gender: 'Female',
+        email: 'bhumikalama@gmail.com',
+        phone: '9811276221',
+        address: 'Swayambhu',
+        username: 'bhumikalama44',
+        action:<div><button className="editbtn"> editbtn </button> <button className="deletebtn"> deletebtn </button> </div>,
+
+      },
+    ]
     
-      return (
-        <MDBDataTable
-          striped
-          hover
-          data={data}
-        />
-      );
+  };
+
+  return (
+    <div>
+
+      <div>
+        <h3 className="adminpage-headers mb-4"> List of Users </h3>
+      </div>
+      <MDBDataTable
+        striped
+        hover
+        data={data}
+      />
+
+    </div>
+  )
+
 }
 
 export default ViewUser;
