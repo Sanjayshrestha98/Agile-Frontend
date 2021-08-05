ab7e26d12c47a5d9aa1d3d5ca3982402493f7a67
Sanjayshrestha98 <sanjayshrestha969@gmail.com>
2021-08-05 12:30:05 +0545

    faq 1

--- src/Container/FAQ.js
+++ src/Container/FAQ.js
@@ -6,13 +6,74 @@ import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
+    },
+}));
+
+
 function FAQ() {
 
-  return(
-      <div>
+    const classes = useStyles();
 
-      </div>
-  )
+
+    return (
+        <div className="faqsection">
+
+            <div className={classes.root}>
+
+                <div className="title">
+                    Freqently Asked Questions
+                </div>
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography className={classes.heading}>What is Your Name</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            My Name is Sanjay Shrestha
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel2a-content"
+                        id="panel2a-header"
+                    >
+                        <Typography className={classes.heading}>Accordion 2</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                            sit amet blandit leo lobortis eget.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+
+            </div>
+
+            <div className="ask">
+                <div className="title"> Ask A Question </div>
+                <input type="text"/>
+                <button>
+                    Ask
+                </button>
+            </div>
+        </div>
+
+    )
 }
 
 export default FAQ
