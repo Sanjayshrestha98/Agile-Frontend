5044ab162f044e380b61f6ff2b75fa3cc655652d
Sanjayshrestha98 <sanjayshrestha969@gmail.com>
2021-08-15 21:18:30 +0545

    theme

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -183,6 +183,14 @@ function Header() {
 							</Nav>
 
 							<Nav className="rightnav">
+
+							<Nav.Link>
+									<div data-tip="Profile" className="righticons">
+										<button onClick={() => themeToggler()}>Change Theme</button>
+
+									</div>
+								</Nav.Link>
+								
 								<Nav.Link href="/register">Register</Nav.Link>
 								<Nav.Link href="/login">Login</Nav.Link>
 
